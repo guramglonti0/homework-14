9e0b7b7181b0c9ca971c0571cbab4e78a083c0e9
guramglonti0 <112972407+guramglonti0@users.noreply.github.com>
2022-11-01 15:53:20 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var cinema = {
+    name:"Indoor Cinema", 
+    languages: ["ქართული" , "ინგლისური" , "ინგლისური ქართული სუბტიტრებით "],
+    schedule:{
+    monday:"10:00 - 24:00",
+    tuesday:"10:00 - 24:00",
+    wednesday:"10:00 - 24:00",
+    thursday:"10:00 - 24:00",
+    friday:"10:00 - 24:00",
+    saturday:"10:00 - 02:00",
+    sunday:"10:00 - 02:00",
+    }, 
+    movieOne :{
+         nameOne:"RocknRolla", PremieraOne:"Monday - 22:00-24:00" 
+     } ,
+    movieTwo :{
+         nameTwo:"Snatch",  PremieraTwo:" Friday- 22:00-24:00",
+     } ,
+    movieThree :{
+         nameThree:"Lock, Stock and Two Smoking Barrels",  PremieraThree:"Sunday - 22:00-24:00"
+     } ,
+     filmDirector:"Movies from Guy Ritchie" 
+    }  
+    console.log(cinema)
+    console.log(cinema.movieOne)
+    console.log(cinema.movieTwo)
+    console.log(cinema.movieThree) 
+    console.log(cinema.filmDirector)
